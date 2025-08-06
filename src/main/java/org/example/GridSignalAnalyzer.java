ffdf27807245efb6212697183419287aafb7d85c
Andyzxc4 <lacraandre111@gmail.com>
2025-08-06 15:55:10 +0800

    refactor: refactor code to meet lab requirements

--- src/main/java/org/example/GridSignalAnalyzer.java
+++ src/main/java/org/example/GridSignalAnalyzer.java
@@ -1,43 +1,43 @@
 package org.example;
 
 
-import java.util.Arrays;
-
 public class GridSignalAnalyzer {
-    public static void main(String[] args) {
+    // Mission Data: The Scrambled Communications Grid
+    char[][] scrambledGrid = {
+            {'C', 'H', 'A', 'R', 'L', 'I', 'E'},
+            {'A', 'L', 'P', 'H', 'A'},
+            {'T', 'A', 'N', 'G', 'O', '7'},
+            {'B', 'R', 'A', 'V', 'O'}
+    };
 
-        // Mission Data: The Scrambled Communications Grid
-        char[][] scrambledGrid = {
-                {'C', 'H', 'A', 'R', 'L', 'I', 'E'},
-                {'A', 'L', 'P', 'H', 'A'},
-                {'T', 'A', 'N', 'G', 'O', '7'},
-                {'B', 'R', 'A', 'V', 'O'}
-        };
+    //  main method
+    public static void main(String[] args) {
+        GridSignalAnalyzer analyzer = new GridSignalAnalyzer();
 
         System.out.println("--- PHASE 2: STANDARD FOR-LOOP MANEUVER ---");
-        analyzeWithForLoop(scrambledGrid);
+        analyzer.analyzeWithForLoop();
 
         System.out.println("\n--- PHASE 3: ADVANCED FOR-EACH SWEEP ---");
-        analyzeWithForEachLoop(scrambledGrid);
+        analyzer.analyzeWithForEachLoop();
 
         System.out.println("\n--- PHASE 4: HYBRID LOOP APPROACH ---");
-        analyzeWithHybridLoop(scrambledGrid);
+        analyzer.analyzeWithHybridLoop();
 
     }
 
     //  phase 2
-    public static void analyzeWithForLoop(char[][] scrambleGrid) {
-        for(int i = 0; i < scrambleGrid.length; i++) {
-            for(int j = 0; j < scrambleGrid[i].length; j++) {
-                System.out.print(scrambleGrid[i][j] + " ");
+    public void analyzeWithForLoop() {
+        for(int i = 0; i < this.scrambledGrid.length; i++) {
+            for(int j = 0; j < this.scrambledGrid[i].length; j++) {
+                System.out.print(this.scrambledGrid[i][j] + " ");
             }
             System.out.println(" ");
         }
     }
 
     // phase 3
-    public static void analyzeWithForEachLoop(char[][] scrambleGrid) {
-        for (char[] rowArray : scrambleGrid) {
+    public void analyzeWithForEachLoop() {
+        for(char[] rowArray : this.scrambledGrid) {
             System.out.println("Processing Row with Hash: " + rowArray);
             for (char aChar : rowArray) {
                 System.out.println("-> " + aChar);
@@ -46,8 +46,8 @@ public class GridSignalAnalyzer {
     }
 
     // phase 4
-    public static void analyzeWithHybridLoop(char[][] scrambleGrid) {
-        for (char[] outerArr : scrambleGrid) {
+    public void analyzeWithHybridLoop() {
+        for(char[] outerArr : this.scrambledGrid) {
             for(int i = 0; i < outerArr.length; i++) {
                 System.out.print(outerArr[i] + " ");
             }
